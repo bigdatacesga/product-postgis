dc38b985af515b2cd1db044ef107c5a47f1cab18
JfuentesR <infojose20@gmail.com>
2021-04-19 13:35:02 +0200

    typo on img name

--- register.sh
+++ register.sh
@@ -2,7 +2,7 @@
 # export TOKEN='<TOKEN>'
 # export AUTH="x-auth-token: $TOKEN"
 
-http POST http://paas:6000/bigdata/api/v1/products name=postgis version=25_12_BETA description='BETA VERSION Postgresql with the most common extension for spatial data' logo_url='http://bigdata.cesga.es/img/postgis_logo_BETA.gif' x-auth-token:$TOKEN
+http POST http://paas:6000/bigdata/api/v1/products name=postgis version=25_12_BETA description='BETA VERSION Postgresql with the most common extension for spatial data' logo_url='http://bigdata.cesga.es/img/postgis_logo_BETA.png' x-auth-token:$TOKEN
 curl -X PUT http://paas:6000/bigdata/api/v1/products/postgis/25_12_BETA/template --data-binary @template.json -H "Content-type: application/json" -H "$AUTH"
 curl -X PUT http://paas:6000/bigdata/api/v1/products/postgis/25_12_BETA/options --data-binary @options.json -H "$AUTH"
 curl -X PUT http://paas:6000/bigdata/api/v1/products/postgis/25_12_BETA/orchestrator --data-binary @fabfile.py -H "$AUTH"
