16b287452499efba5e22e5725cfd749b3da1a0bb
JfuentesR <infojose20@gmail.com>
2021-04-19 13:25:15 +0200

    beta register.sh

--- register.sh
+++ register.sh
@@ -2,11 +2,11 @@
 # export TOKEN='<TOKEN>'
 # export AUTH="x-auth-token: $TOKEN"
 
-http POST http://paas:6000/bigdata/api/v1/products name=postgis version=25_12 description='Postgresql with the most common extension for spatial data' logo_url='http://bigdata.cesga.es/img/postgis_logo.gif' x-auth-token:$TOKEN
-curl -X PUT http://paas:6000/bigdata/api/v1/products/postgis/25_12/template --data-binary @template.json -H "Content-type: application/json" -H "$AUTH"
-curl -X PUT http://paas:6000/bigdata/api/v1/products/postgis/25_12/options --data-binary @options.json -H "$AUTH"
-curl -X PUT http://paas:6000/bigdata/api/v1/products/postgis/25_12/orchestrator --data-binary @fabfile.py -H "$AUTH"
+http POST http://paas:6000/bigdata/api/v1/products name=postgis version=25_12_BETA description='BETA VERSION Postgresql with the most common extension for spatial data' logo_url='http://bigdata.cesga.es/img/postgis_logo_BETA.gif' x-auth-token:$TOKEN
+curl -X PUT http://paas:6000/bigdata/api/v1/products/postgis/25_12_BETA/template --data-binary @template.json -H "Content-type: application/json" -H "$AUTH"
+curl -X PUT http://paas:6000/bigdata/api/v1/products/postgis/25_12_BETA/options --data-binary @options.json -H "$AUTH"
+curl -X PUT http://paas:6000/bigdata/api/v1/products/postgis/25_12_BETA/orchestrator --data-binary @fabfile.py -H "$AUTH"
 
 # Launching
-# http POST http://paas:6000/bigdata/api/v1/products/postgis/25_12 password="verysecret" x-auth-token:$TOKEN
+# http POST http://paas:6000/bigdata/api/v1/products/postgis/25_12_BETA password="verysecret" x-auth-token:$TOKEN
 
