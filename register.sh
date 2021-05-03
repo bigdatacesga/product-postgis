fdc2591e0a99ad1ba3e7cdd654d026bda32f7188
JfuentesR <infojose20@gmail.com>
2021-05-03 13:54:52 +0200

    working spatiotemporal pgsql

--- register.sh
+++ register.sh
@@ -2,7 +2,7 @@
 # export TOKEN='<TOKEN>'
 # export AUTH="x-auth-token: $TOKEN"
 
-http POST http://paas:6000/bigdata/api/v1/products name=postgis version=25_12 description='Postgresql with the most common extension for spatial data' logo_url='http://bigdata.cesga.es/img/postgis_logo.gif' x-auth-token:$TOKEN
+http POST http://paas:6000/bigdata/api/v1/products name=postgis version=12.6 description='Postgresql + Postgis & Timescaledb. Postgresql with the most common extensions for spatial data and time series' logo_url='http://bigdata.cesga.es/img/pgsql_sp-tmp_logo_transparent.png' x-auth-token:$TOKEN
 curl -X PUT http://paas:6000/bigdata/api/v1/products/postgis/25_12/template --data-binary @template.json -H "Content-type: application/json" -H "$AUTH"
 curl -X PUT http://paas:6000/bigdata/api/v1/products/postgis/25_12/options --data-binary @options.json -H "$AUTH"
 curl -X PUT http://paas:6000/bigdata/api/v1/products/postgis/25_12/orchestrator --data-binary @fabfile.py -H "$AUTH"
